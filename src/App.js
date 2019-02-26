b649c30b041a61a6236a0ffcd7ceeaed52d150f8
bencode <bencode@163.com>
2019-02-26 15:48:54 +0800

    move

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import SortList from 'pages/SortList';
 import MyTabs from 'pages/MyTabs';
 import Forms from 'pages/Forms';
