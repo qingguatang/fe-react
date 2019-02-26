c84a29a61af8990dffcb71e9f3712b4311db2b76
bencode <bencode@163.com>
2019-02-26 15:55:20 +0800

    update

---
+++ docs/precache-manifest.0e14abebfd93179a14e5d1c93502811e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7321600a4397bd1fd4f4",
+    "url": "/fe-react/static/css/main.1c5ce69a.chunk.css"
+  },
+  {
+    "revision": "7321600a4397bd1fd4f4",
+    "url": "/fe-react/static/js/main.7321600a.chunk.js"
+  },
+  {
+    "revision": "31baf23a0e1401b443a1",
+    "url": "/fe-react/static/css/1.c53854af.chunk.css"
+  },
+  {
+    "revision": "31baf23a0e1401b443a1",
+    "url": "/fe-react/static/js/1.31baf23a.chunk.js"
+  },
+  {
+    "revision": "1d2cd9c2e1eaf0aa743b",
+    "url": "/fe-react/static/js/runtime~main.1d2cd9c2.js"
+  },
+  {
+    "revision": "8923fe81ffe45d6c12c6c18f3064b53b",
+    "url": "/fe-react/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20215faf69e571e78394774918151546.js"
+  "/fe-react/precache-manifest.0e14abebfd93179a14e5d1c93502811e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fe-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
